c5f9dfa57b14b85bd34196e99b48c583c0313bcb
Patrick <ptyrawl@gmail.com>
2014-05-23 17:12:09 -0400

    Added powershell profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+# Powershell profile
+set-ExecutionPolicy unrestricted
+$Shell = $Host.UI.RawUI
+$monitor = Get-WmiObject -Class Win32_DesktopMonitor
+$size = $Shell.BufferSize
+$size.width=$monitor.ScreenWidth
+$size.height=$monitor.ScreenHeight
+$Shell.BufferSize = $size
+$size = $Shell.WindowSize
+$size.height=25
+$size.width=80
+$Shell.WindowSize = $size
+$shell.BackgroundColor = "Black"
+$shell.ForegroundColor = "White"
+set-location c:\
+
+# Program Aliases
+new-item alias:np -value C:\Windows\System32\notepad.exe
+
+# Clear powershell window for a clean workspace on launch
+Clear-Host
